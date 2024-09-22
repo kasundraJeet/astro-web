91ed6b9529c95f466ced90b754f9db53165001ec
kasundraJeet <kasundrajeet29@gmail.com>
2024-09-22 10:11:09 +0530

    added new packnege for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
